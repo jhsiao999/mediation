88ac88baa1908d77ea8754d28a4c4077388b4083
jhsiao999 <joyce.hsiao1@gmail.com>
2018-02-21 12:19:43 -0600

    add regressing out approach

---
+++ pkg/R/mediate.test.regressing.R
@@ -0,0 +1,111 @@
+#' @title An ad hoc approach for testing mediation effect
+#'
+#' @description Different from the joint modeling approach, the direct effect is estimated using data after regressing the effect of mediating variable out from the dependent variable. In addition, voom transformation is applied to the expression data for estimating the condition effect.
+#'
+#' @param Y gene by sample expression matrix (G by N). Required to be normalized and log transformed.
+#' @param X sample condition labels, assumed to be binary for now.
+#' @param M data for the mediating variable, a gene by sample matrix (G by N).
+#'
+#'
+#' @examples
+#' #library(medinome)
+#' #library(limma)
+#' #df <- get(load(file="../data/example-kidney-v-heart-human.rda"))
+#' #Y <- df$exprs_pair
+#' #M <- df$methyl_pair
+#' #X <- df$tissue
+#' #fit <- mediate.test.regressing(Y=Y, X=X, M=M)
+#' #ash_reg <- ash(betahat=fit$d, sebetahat=fit$d_se, lik=lik_normal())
+#'
+#' @export
+
+mediate.test.regressing <- function(Y, X, M) {
+
+  library(limma)
+  library(assertthat)
+
+  # evaluate argument conditions
+  assert_that(all.equal(dim(Y), dim(M)))
+  assert_that(all.equal(length(unique(X)), 2))
+  assert_that(all.equal(length(X), dim(Y)[2]))
+
+  G <- nrow(Y)
+
+  # model 1: Y_g ~ \tau_g X
+  # specify tissue coding
+  design_1 <- model.matrix(~X)
+
+  Y_counts <- 2^Y
+  Y_voom <- voom(Y_counts, design=design_1, normalize.method = "none")
+
+  model_1 <- lmFit(Y_voom, design_1)
+  model_1 <- eBayes(model_1)
+
+  # model 2: Y_g ~ \kappa_g M_g
+  # to regress out the effect of mediating variable from Y
+  Y_resid <- array(0, dim = dim(Y))
+  for (g in 1:G){
+    Y_resid[g,] <- lm(t(Y[g, ]) ~ t(M[g, ]))$resid
+  }
+  rownames(Y_resid) <- rownames(Y)
+
+  # model 3: Y_resid_g ~ \tau^{\prime}_g X
+  model_3 <- lmFit(Y_resid, design_1)
+
+
+  # get effect sizes
+  beta1 <- coef(model_1[,2])
+  beta3 <- coef(model_3[,2])
+
+  se_beta1 <- se_beta2 <- se_beta3 <- cov_beta13 <- vector("numeric", G)
+
+  # <---- get variances of \tau_g (condition effect)
+  se_beta1 <- model_1$sigma*sqrt(model_1$cov.coefficients[2,2])
+
+  # <---- get variances of \kappa_g mediator variable effect
+  for (g in 1:length(se_beta2)) {
+    design_2g <- model.matrix(~ as.numeric(M[g,]))
+    sigma_g <- model_1$sigma[g]
+    se_beta2[g] <- sigma_g*sqrt((solve(t(design_2g)%*%design_2g))[2,2])
+  }
+
+  # <---- get variances of \tau^{\prime}_g condition effect on expression after
+  # regressing out mediating variable
+  A <- solve(t(design_1)%*%design_1)%*%t(design_1)
+  contr.vector <- array(c(0,1), dim=c(2,1))
+
+  # compute beta3 by hand
+  for (g in 1:length(se_beta3)) {
+    M_g <- t(M[g,])
+    design_2g <- model.matrix(~ as.numeric(M_g))
+    A_2g <- solve(t(design_2g)%*%design_2g)%*%t(design_2g)
+    sigma_g <- model_1$sigma[g]
+    var_beta2g <- (se_beta2^2)[g]
+    var_part1 <- (se_beta1[g])^2
+    var_part2 <- ( A%*%M_g%*%var_beta2g%*%t(M_g)%*%t(A) )[2,2]
+    var_part3 <- ( 2*(sigma_g^2)*A%*%t(A_2g)%*%contr.vector%*%t(M_g)%*%t(A) )[2,2]
+    se_beta3[g] <- sqrt(var_part1 + var_part2 + var_part3)
+  }
+
+  # cov(beta1,beta3)
+  for (g in 1:length(cov_beta13)) {
+    M_g <- t(M[g,])
+    design_2g <- model.matrix(~ as.numeric(M_g))
+    A_2g <- solve(t(design_2g)%*%design_2g)%*%t(design_2g)
+    sigma_g <- model_1$sigma[g]
+    var_part1 <- (se_beta1[g])^2
+    cov_beta13[g] <- var_part1 - (sigma_g^2)*((A %*% t(A_2g) %*%contr.vector%*%t(M_g)%*%t(A))[2,2])
+  }
+
+  # cov(beta1-beta3)
+  d_se <- sqrt(se_beta1^2 + se_beta3^2 - 2*cov_beta13)
+  d <- beta1-beta3
+
+  df <- data.frame(d=as.numeric(d), d_se=d_se, tau = as.numeric(beta1),
+                   tau_prime = as.numeric(beta3))
+  rownames(df) <- rownames(Y)
+  return(df)
+}
+
+
+
---
+++ pkg/R/mediate.test.voom.R
@@ -0,0 +1,64 @@
+#' @title Mediation test for data after voom transformation
+#'
+#' @description Compute statistics for evaluating mediating effect in genomewide expression data. Apply voom transform to log2 expression data.
+#'
+#' @param Y gene by sample expression matrix (G by N). Required to be normalized and log transformed.
+#' @param X sample condition labels, assumed to be binary for now.
+#' @param M data for the mediating variable, a gene by sample matrix (G by N).
+#'
+#' @export
+#'
+mediate.test <- function(Y, X, M) {
+
+  library(limma)
+  library(assertthat)
+
+  # evaluate argument conditions
+  assert_that(all.equal(dim(Y), dim(M)))
+  assert_that(all.equal(length(unique(X)), 2))
+  assert_that(all.equal(length(X), dim(Y)[2]))
+
+  G <- dim(Y)[1]
+  N <- dim(Y)[2]
+
+  Y <- as.matrix(Y)
+  M <- as.matrix(M)
+  X <- factor(X)
+
+  # Fitting model 1: Y_g = \gamma_{1g} + \tau_g X + \epsilon_{1g}
+  # First transform the log2 expression to counts and
+  # compute voom weights
+  design_1 <- model.matrix(~X)
+  Y_counts <- 2^Y
+  Y_voom <- voom(Y_counts, design=design_1, normalize.method = "none")
+  model_1 <- lmFit(Y_voom, design_1)
+  model_1 <- eBayes(model_1)
+
+  # Fitting model 2: M_g = \gamma_{2g} + \alpha X + \epsilon_{2g}
+  design_2 <- model.matrix(~X)
+  model_2 <- lmFit(M, design_2)
+
+  # Fitting model 3: M_g = \gamma_{3g} + \tau^{\prime}_g X + \beta_g M_g \epsilon_{3g}
+  model_3 <- lm.varyingCovariates(Y=Y, X=X, M=M)
+
+  tau <- coef(model_1)[,2]
+  tau_prime <- model_3$coefs[,2]
+  alpha <- coef(model_2)[,2]
+  beta <- model_3$coefs[,3]
+  sigma_alpha <- model_2$stdev.unscaled[,2]*model_2$sigma
+  sigma_beta <- model_3$stdev.unscaled[,3]*model_3$sigma
+
+  d <- tau-tau_prime
+  se <- sqrt((alpha*sigma_beta)^2 + (beta*sigma_alpha)^2)
+  return(list(d=d,
+              se=se,
+              tau=tau,
+              tau_prime=tau_prime,
+              alpha=alpha,
+              beta=beta,
+              sigma_alpha=sigma_alpha,
+              sigma_beta=sigma_beta
+  ))
+}
+
+
